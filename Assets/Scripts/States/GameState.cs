ff39ceca5c5409075394d330fb35104c63d93a92
Walt Mitchell <walt@walt.codes>
2017-12-24 14:11:15 -0700

    fix old bracket style

--- Assets/Scripts/States/GameState.cs
+++ Assets/Scripts/States/GameState.cs
@@ -17,10 +17,8 @@ class GameState : MonoBehaviour {
         ground = (GameObject)GameObject.Instantiate(Resources.Load("Ground"), new Vector3(0, 0, 0), Quaternion.identity);
 
         // Make spheres
-        for (int y = -5; y < 0; y++)
-        {
-            for (int x = -6; x <= 5; x++)
-            {
+        for (int y = -5; y < 0; y++) {
+            for (int x = -6; x <= 5; x++) {
                 Vector3 vector = new Vector3((x * 3) + (Random.value * 2) - 1, (y * -3) + (Random.value * 2) - 1, (Random.value * 24) - 12);
                 GameObject.Instantiate(Resources.Load("Sphere"), vector, Quaternion.identity);
             }
@@ -32,8 +30,7 @@ class GameState : MonoBehaviour {
     {
         countdown = 3;
 
-        while (countdown > 0)
-        {
+        while (countdown > 0) {
             Debug.Log("Playing for " + countdown + "...");
             countdown--;
             yield return new WaitForSecondsRealtime(1);
